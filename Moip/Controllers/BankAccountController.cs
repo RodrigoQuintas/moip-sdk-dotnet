8c7bee552a5c525edc857906bc40c5518eb9ecff
RodrigoQuintas <rodrigo.santos.quintas@gmail.com>
2018-02-15 17:25:49 -0200

    Bank Account and Transfer

---
+++ Moip/Controllers/BankAccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using Moip;
+using Moip.Utilities;
+using Moip.Http.Request;
+using Moip.Http.Response;
+using Moip.Http.Client;
+using Moip.Exceptions;
+
+namespace Moip.Controllers
+{
+    public class BankAccountController : BaseController
+    {
+        public Models.BankAccountResponse CreateTransfer(Models.BankAccountRequest body, String customerId)
+        {
+            Task<Models.BankAccountResponse> t = CreateTransferAsync(body, customerId);
+            APIHelper.RunTaskSynchronously(t);
+            return t.Result;
+        }
+
+        public async Task<Models.BankAccountResponse> CreateTransferAsync(Models.BankAccountRequest body, String customerId)
+        {
+
+            string _baseUri = Configuration.GetBaseURI();
+
+            StringBuilder _queryBuilder = new StringBuilder(_baseUri);
+            _queryBuilder.Append("/customers/{customer-id}/bankaccounts");
+
+            APIHelper.AppendUrlWithTemplateParameters(_queryBuilder, new Dictionary<string, object>()
+            {
+                { "customer-id", customerId }
+            });
+
+            string _queryUrl = APIHelper.CleanUrl(_queryBuilder);
+
+            var _headers = Utilities.APIHelper.GetHeader();
+
+            var _body = APIHelper.JsonSerialize(body);
+
+            HttpRequest _request = ClientInstance.PostBody(_queryUrl, _headers, _body);
+
+            HttpStringResponse _response = (HttpStringResponse)await ClientInstance.ExecuteAsStringAsync(_request).ConfigureAwait(false);
+            HttpContext _context = new HttpContext(_request, _response);
+
+            base.ValidateResponse(_response, _context);
+
+            try
+            {
+                return APIHelper.JsonDeserialize<Models.BankAccountResponse>(_response.Body);
+            }
+            catch (Exception _ex)
+            {
+                throw new APIException("Failed to parse the response: " + _ex.Message, _context);
+            }
+        }
+    }
+}
---
+++ Moip/Controllers/TransferController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using Moip;
+using Moip.Utilities;
+using Moip.Http.Request;
+using Moip.Http.Response;
+using Moip.Http.Client;
+using Moip.Exceptions;
+
+
+namespace Moip.Controllers
+{
+    public partial class TransferController : BaseController
+    {
+        public Models.TransferResponse CreateTransfer(Models.TransferRequest body)
+        {
+            Task<Models.TransferResponse> t = CreateTransferAsync(body);
+            APIHelper.RunTaskSynchronously(t);
+            return t.Result;
+        }
+
+        public async Task<Models.TransferResponse> CreateTransferAsync(Models.TransferRequest body)
+        {
+
+            string _baseUri = Configuration.GetBaseURI();
+
+            StringBuilder _queryBuilder = new StringBuilder(_baseUri);
+            _queryBuilder.Append("/transfer");
+
+            string _queryUrl = APIHelper.CleanUrl(_queryBuilder);
+
+            var _headers = Utilities.APIHelper.GetHeader();
+
+            var _body = APIHelper.JsonSerialize(body);
+
+            HttpRequest _request = ClientInstance.PostBody(_queryUrl, _headers, _body);
+
+            HttpStringResponse _response = (HttpStringResponse)await ClientInstance.ExecuteAsStringAsync(_request).ConfigureAwait(false);
+            HttpContext _context = new HttpContext(_request, _response);
+
+            base.ValidateResponse(_response, _context);
+
+            try
+            {
+                return APIHelper.JsonDeserialize<Models.TransferResponse>(_response.Body);
+            }
+            catch (Exception _ex)
+            {
+                throw new APIException("Failed to parse the response: " + _ex.Message, _context);
+            }
+        }
+    }
+}
